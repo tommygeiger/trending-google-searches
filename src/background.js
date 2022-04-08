2c67c8ce27a75590a54978e61e1f5be0acadd566
tommygeiger <tommyhawk21@gmail.com>
2022-04-08 17:32:39 -0400

    Moved fetch to background.js and update hourly. Added badge

---
+++ src/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  fetchRSS();
+  var id = setInterval(fetchRSS, 36e5);
+  console.log("Installed, interval set.");
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  fetchRSS();
+  var id = setInterval(fetchRSS, 36e5);
+  console.log("Started, interval set.");
+});
+
+function fetchRSS() {
+  fetch("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
+  .then(response => response.text())
+  .then(text => {
+    chrome.storage.local.set({ text });
+    chrome.action.setBadgeBackgroundColor({ color: "#63aa56"})
+    chrome.action.setBadgeText({text: "1"});
+  });
+}
--- src/popup.js
+++ src/popup.js
@@ -1,79 +1,79 @@
-const rssUrl = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=US";
-const today = new Date();
+chrome.storage.local.get("text", ({ text }) => {
+  chrome.action.setBadgeText({text: ""});
 
-fetch(rssUrl)
-  .then(response => response.text())
-  .then(text => new DOMParser().parseFromString(text, "text/xml"))
-  .then(xmlDoc => {
+  const today = new Date();
+  const xmlDoc = new DOMParser().parseFromString(text, "text/xml");
 
-    let items = xmlDoc.querySelectorAll("item");
-    let html = "";
-    let i = 1;
+  let items = xmlDoc.querySelectorAll("item");
+  let html = "";
+  let i = 1;
 
-    html += `
-      <div class="headerParent">
-        <table class="header">
-          <tr>
+  html += `
+    <div class="headerParent">
+      <table class="header">
+        <tr>
+          <td>
+            <h3 class="headerText" id="infoButton">?</h3>
+          </td>
+          <td class="center">
+            <h3 class="headerText">
+              ${today.toLocaleDateString("en-Us", {weekday:'long', month:'long', day:'numeric'})}
+            </h3>
+          </td>
+          <td class="right">
+            <a href='https://buymeacoff.ee/tommygeiger' target='_blank'>
+              <img class="headerImg" src="/assets/coffee.png" alt="Buy Me a Coffee"/>
+            </a>
+          </td>
+        </tr>
+      </table>
+      <div id="infoDiv" style="display:none;">
+        <hr>
+        <article>
+          <p class="info">
+          Under a Rock reads Google's Daily Search Trends RSS feed, highlighting Google searches that jumped significantly in traffic over the past 24 hours, with hourly updates. Each trend shows the query that was searched, the number of searches made, and a relevent news article.
+          <a href="https://trends.google.com/trends/trendingsearches/daily?geo=US" target="_blank">Learn More</a>
+          </p>
+        </article>
+      </div>
+      <hr>
+    </div>
+    `;
+
+  items.forEach(elem => {
+    html +=`
+      <article>
+        <table>
+          <col style="width:5%">
+          <col>
+          <col style="width:11%"></col>
+          <tr class="topRow">
+            <td class="center">${i}</td>
             <td>
-              <h3 class="headerText" id="infoButton">?</h3>
+              <a class="articleTitle" href="https://www.google.com/search?q=${elem.querySelector("title").innerHTML}" target="_blank">${decodeString(elem.querySelector("title").innerHTML)}</a>
             </td>
-            <td class="center">
-              <h3 class="headerText">
-                ${today.toLocaleDateString("en-Us", {weekday:'long', month:'long', day:'numeric'})}
-              </h3>
-            </td>
-            <td class="right">
-              <a href='https://buymeacoff.ee/tommygeiger' target='_blank'>
-                <img class="headerImg" src="/assets/coffee.png" alt="Buy Me a Coffee"/>
-              </a>
+            <td class="center">${abbreviateNumber(elem.querySelector("approx_traffic").innerHTML)}</td>
+          </tr>
+          <tr class="bottomRow">
+            <td></td>
+            <td>
+              <a href="${elem.querySelector("news_item_url").innerHTML} "target="_blank">${decodeString(elem.querySelector("news_item_title").innerHTML)}</a>
+              <span>${Math.floor(Math.abs(today - new Date(elem.querySelector("pubDate").innerHTML)) / 36e5)}h ago</span>
             </td>
           </tr>
         </table>
-        <div id="infoDiv" style="display:none;">
-          <hr>
-          <article>
-            <p class="info">
-            Under a Rock reads Google's Daily Search Trends RSS feed, which highlights Google searches that jumped significantly in traffic over the past 24 hours, and updates hourly. Each trend shows the query that was searched, the number of searches made, and the top relevent news article.
-            </p>
-          </article>
-        </div>
-        <hr>
-      </div>
-      `;
+      </article>
+    `;
 
-    items.forEach(elem => {
-      html +=`
-        <article>
-          <table>
-            <col style="width:5%">
-            <col>
-            <col style="width:11%"></col>
-            <tr class="topRow">
-              <td class="center">${i}</td>
-              <td>
-                <a class="articleTitle" href="https://www.google.com/search?q=${elem.querySelector("title").innerHTML}" target="_blank">${decodeString(elem.querySelector("title").innerHTML)}</a>
-              </td>
-              <td class="center">${abbreviateNumber(elem.querySelector("approx_traffic").innerHTML)}</td>
-            </tr>
-            <tr class="bottomRow">
-              <td></td>
-              <td>
-                <a href="${elem.querySelector("news_item_url").innerHTML} "target="_blank">${decodeString(elem.querySelector("news_item_title").innerHTML)}</a>
-                <span>${Math.floor(Math.abs(today - new Date(elem.querySelector("pubDate").innerHTML)) / 36e5)}h ago</span>
-              </td>
-            </tr>
-          </table>
-        </article>
-      `;
-      if(i < 20){
-        html +="<hr>";
-        i++;
-      }
-    });
+    if(i < 20) {
+      html +="<hr>"; i++;
+    }
 
-    document.body.insertAdjacentHTML("beforeend", html);
-    document.getElementById("infoButton").addEventListener("click", toggleInfo);
   });
+
+  document.body.insertAdjacentHTML("beforeend", html);
+  document.getElementById("infoButton").addEventListener("click", toggleInfo);
+});
 
 function abbreviateNumber(string) {
   let number = parseInt(string.replace(/\D/g,""));
